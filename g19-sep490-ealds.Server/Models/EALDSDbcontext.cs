7911accc9fddbc130e237f3efd22a85151a810d8
phucch11 <153418387+LongSuy@users.noreply.github.com>
2026-03-23 22:29:28 +0700

    Update DB Context

--- g19-sep490-ealds.Server/Models/EALDSDbcontext.cs
+++ g19-sep490-ealds.Server/Models/EALDSDbcontext.cs
@@ -585,6 +585,8 @@ public partial class EaldsDbContext :
                 .HasDefaultValueSql("(getdate())")
                 .HasColumnType("datetime");
             entity.Property(e => e.PlannedDate).HasColumnType("datetime");
+            entity.Property(e => e.EstimatedCost).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.ExpectedCompletionDate).HasColumnType("datetime");
 
             entity.HasOne(d => d.Asset).WithMany(p => p.MaintenaceTasks)
                 .HasForeignKey(d => d.AssetId)
@@ -719,6 +721,7 @@ public partial class EaldsDbContext :
 
             entity.Property(e => e.ActualCost).HasColumnType("decimal(18, 2)");
             entity.Property(e => e.RepairDate).HasColumnType("datetime");
+            entity.Property(e => e.DamageDate).HasColumnType("datetime");
 
             entity.HasOne(d => d.Supplier).WithMany(p => p.RepairRecords)
                 .HasForeignKey(d => d.SupplierId)
@@ -737,6 +740,8 @@ public partial class EaldsDbContext :
             entity.ToTable("RepairTask");
 
             entity.Property(e => e.EstimatedCost).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.RepairDate).HasColumnType("datetime");
+            entity.Property(e => e.ExpectedCompletionDate).HasColumnType("datetime");
 
             entity.HasOne(d => d.Asset).WithMany(p => p.RepairTasks)
                 .HasForeignKey(d => d.AssetId)
