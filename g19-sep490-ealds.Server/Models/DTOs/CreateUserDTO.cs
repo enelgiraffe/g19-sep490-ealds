1f0df3985efe879966a52426aa444156cf7c91b2
Nam <52032842+Boishacker@users.noreply.github.com>
2026-03-29 11:48:03 +0700

    fix phone number

--- g19-sep490-ealds.Server/Models/DTOs/CreateUserDTO.cs
+++ g19-sep490-ealds.Server/Models/DTOs/CreateUserDTO.cs
@@ -23,10 +23,6 @@ public class CreateUserDTO
     public string Password { get; set; } = null!;
 
     [Required]
-    [MaxLength(50)]
-    public string Phone { get; set; } = null!;
-
-    [Required]
     public int DepartmentId { get; set; }
 
     public string? ImageUrl { get; set; }
@@ -34,7 +30,8 @@ public class CreateUserDTO
     public int Status { get; set; } = 1;
 
     // SĐT: tuỳ chọn, nếu cung cấp phải đúng định dạng Việt Nam (10 số, bắt đầu bằng 0)
-    [RegularExpression(@"^0\d{9}$", ErrorMessage = "Số điện thoại không hợp lệ (phải gồm 10 chữ số và bắt đầu bằng 0).")]
+    [MaxLength(50)]
+    [RegularExpression(@"^$|^0\d{9}$", ErrorMessage = "Số điện thoại không hợp lệ (phải gồm 10 chữ số và bắt đầu bằng 0).")]
     public string? Phone { get; set; }
 
     public List<int> RoleIds { get; set; } = new List<int>();
