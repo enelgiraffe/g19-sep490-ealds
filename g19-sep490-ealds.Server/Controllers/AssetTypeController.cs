8038d191eac55905e6564859d44f06b41b9523ba
enel <139697224+enelgiraffe@users.noreply.github.com>
2026-05-04 16:30:52 +0700

    refactor: finalize

--- g19-sep490-ealds.Server/Controllers/AssetTypeController.cs
+++
@@ -1,3 +0,0 @@
-// This controller is superseded by AssetTypesController (REST-style routes at api/AssetTypes).
-// Kept as an empty file to avoid breaking git history; the class has been removed.
-namespace g19_sep490_ealds.Server.Controllers;
